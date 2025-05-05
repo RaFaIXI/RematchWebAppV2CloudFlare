dca80073a31c39dac0cda9d611c1fd3a5307d418
RaFaIXI <129433000+RaFaIXI@users.noreply.github.com>
2025-05-05 20:44:08 +0200

    feat: add favicon and soccer ball images; update Tailwind CSS configuration and global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,43 @@
-import type { Config } from 'tailwindcss';
+// tailwind.config.js
+const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
-  darkMode: 'class',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}', // IMPORTANT: Add this if your code is in /src
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        card: 'hsl(var(--card))',
+        'card-foreground': 'hsl(var(--card-foreground))',
+        popover: 'hsl(var(--popover))',
+        'popover-foreground': 'hsl(var(--popover-foreground))',
+        primary: 'hsl(var(--primary))',
+        'primary-foreground': 'hsl(var(--primary-foreground))',
+        secondary: 'hsl(var(--secondary))',
+        'secondary-foreground': 'hsl(var(--secondary-foreground))',
+        muted: 'hsl(var(--muted))',
+        'muted-foreground': 'hsl(var(--muted-foreground))',
+        accent: 'hsl(var(--accent))',
+        'accent-foreground': 'hsl(var(--accent-foreground))',
+        destructive: 'hsl(var(--destructive))',
+        'destructive-foreground': 'hsl(var(--destructive-foreground))',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+      },
+      fontFamily: {
+        sans: ['Arial', ...fontFamily.sans],
+      },
+    },
+  },
   plugins: [],
-  theme: {},
-} satisfies Config;
+};
